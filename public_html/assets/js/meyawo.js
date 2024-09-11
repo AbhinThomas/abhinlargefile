e048b1849d8560abe3e9063fba8e29d3e2200fbf
AbhinThomas <abhinthomas702@gmail.com>
2024-09-11 06:54:04 +0530

    Update meyawo.js

--- public_html/assets/js/meyawo.js
+++ public_html/assets/js/meyawo.js
@@ -36,3 +36,20 @@ $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
+$("#submit-form").submit((e)=>{
+    e.preventDefault()
+    $.ajax({
+        url:"https://script.google.com/macros/s/AKfycbwzgMb-26YItYcnNiSGfacmvNHdthDZyRg7e0djQUqFt_hoyIa-PvDugdcEeLAYG45C/exec",
+        data:$("#submit-form").serialize(),
+        method:"post",
+        success:function (response){
+            alert("Form submitted successfully")
+            window.location.reload()
+            //window.location.href="https://google.com"
+        },
+        error:function (err){
+            alert("Something Error")
+
+        }
+    })
+})
